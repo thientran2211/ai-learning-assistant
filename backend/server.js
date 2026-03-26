72d2231f92e76fb1957a9b1f4e97919346c26525
Thien Tran <thienforever2211@gmail.com>
2026-03-27 00:57:28 +0700

    Connecting to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import express from 'express';
 import cors from 'cors';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import connectDB from './config/db.js';
 import errorHandler from './middleware/errorHandler.js';
 
 // ES6 module __dirname alternative
